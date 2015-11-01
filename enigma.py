07bfdac94b18a33af796ec46d7c9ae316c2e5dc9
Himanshu Taneja <027himanshu@gmail.com>
2015-11-01 19:25:48 +0530

    minor edit

--- enigma.py
+++ enigma.py
@@ -3,10 +3,9 @@
 Enigma machine modified to work with image files (or rather any 2d array)
 
 For an 8-bit image and 2 rotors we have 2*255*255=130050 combinations
-With 10 pairs of plugboard wires we have C(255,2)*C(253,2)*C(251,2)...*C(235,2) = 144,344,660,914,893,408,208,528,832,440,536,336,416,008,344,832 possible combinations.
-In total (with 10 pairs of plugboard wires and 2 rotors) there are 1.877202315198188773752E+52 combinations for initial setup of rotors and plugboard. 
-However with only 10 pairs of plugboard wires we cannot generate enough entropy (or randomness) to completely encrypt the image,
-therefore in this project I've used the maximum possible pairs of plugboard for an 8 bit image i.e. 256/2=128 pairs.
+Assuming fixed reflectros, that is wiring of reflector cannot be changed.
+Using a maximum possible pairs of plugboard wires (128 pairs for an 8-bit image), We have (C(256,2) * C(254,2) * .... * C(2,2))/123! = 256!/(2^256 * 123!) = 6.099E+224
+Total possible combinations ≈ 10^229 = 2^768, That is 768 bits.
 
 @author: himanshu
 """
